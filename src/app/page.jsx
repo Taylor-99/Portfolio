24da4f39e89603771478272227186e03e6a5cba2
User Taylor <elexus0201@gmail.com>
2024-11-29 19:12:29 -0500

    fixed bugs part 5

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link';
 import HomeNavBar from "./_components/HomeNavBar"
 
 const Home = () => {
--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import NavBar from "../_components/NavBar"
-import Image from 'next/image'
 import Link from 'next/link'
 
 const Resume = () => {
